971ed6d216a53f27a759207d62fc9affbd5ad347
Tatiana Mathis, MBA <13071148+mindfultatiana@users.noreply.github.com>
2025-08-26 22:57:53 -0700

    Updated files

--- example.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\filelocation
-call django_env\Scripts\activate.bat
-cd C:\Users\username\ludIssueTracker
-echo Starting Django development server...
-python manage.py runserver
-pause
